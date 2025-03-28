4dfbde8dfe3df0709b1fae68454bd34c22f24753
CodeIT333 <czellar.botond>
2025-03-28 17:36:58 +0100

    item list filter + separate models

---
+++ APP/Models/ToDOItemCreate.cs
@@ -0,0 +1,9 @@
+﻿namespace APP.Models
+{
+    public class ToDoItemCreate
+    {
+        public string name { get; set; }
+        public string? description { get; set; }
+        public byte priority { get; set; }
+    }
+}
---
+++ APP/Models/ToDoItemPriority.cs
@@ -0,0 +1,9 @@
+﻿namespace APP.Models
+{
+    public enum ToDoItemPriority
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3
+    }
+}
